cc0564ac70fddd88b730e330205fb677ec659cee
Ben Elkin <8xe8n-cm@online.de>
2024-10-06 15:43:04 +0200

    macro @usingany does what is expected to do

--- src/environments.jl
+++ src/environments.jl
@@ -155,7 +155,6 @@ function stdlib_packages()
 end
 export stdlib_packages
 
-
 """
     check_packages(packages; depot = first(DEPOT_PATH)) -> NamedTuple
 
@@ -291,6 +290,42 @@ function make_importable(packages)
 end
 export make_importable
 
+"""
+    @usingany pkg
+    @usingany pkg1, pkg2, ... 
+
+Makes package(s) available, if they are not already, and loads them with `using` keyword. 
+
+- If a package is available in an environment in `LOAD_PATH`, that's OK.
+- If a package is available in a shared environment, this environment will be pushed into `LOAD_PATH`.
+- Otherwise if it can be installed, you will be prompted to select an environment to install the package(s).
+- If the package is not listed in any registry, an error will be thrown.
+"""
+macro usingany(packages)
+
+    if packages isa Symbol
+        packages = [packages]
+    else
+        if packages isa Expr && packages.head == :tuple
+            packages = packages.args
+        else
+            error("The input should be either package name or multiple package names separated by commas")
+        end
+    end
+
+    packages = String.(packages)
+
+    mi = make_importable(packages)
+    mi != :success && error("Some packages could not be installed")
+
+    pkglist = join(packages, ", ")
+
+    q = Meta.parse("using $(pkglist)")
+
+    return q
+end
+export @usingany
+
 function install_shared(p2is::AbstractVector, current_pr)  
     for p2i in p2is
         install_shared(p2i)
@@ -419,7 +454,7 @@ function delete_shared_pkg(s::AbstractString)
 
     p = pkinfos[s]
 
-    p.in_path && error("Package $s is in path. Remove it's environment first.")
+    p.in_path && error("Package $s is in path. Remove it's environment form the path first.")
     
     length(p.envs) > 1 && error("Package $s is present in multiple environments $([env.name for env in p.envs]). Remove it manually.")
     e = p.envs[1]

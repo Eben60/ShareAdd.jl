90e37d68321d9d65e8da9104a82ef754c5a49a5a
Ben Elkin <8xe8n-cm@online.de>
2024-10-13 12:33:27 +0200

    debugging Documentation built

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ include("makedocs.jl")
 # deployment done on the server anyway
 # don't normally run deploydocs here
 deploydocs(
-    repo = "github.com/Eben60/YAArguParser.jl.git",
+    repo = "github.com/Eben60/ShareAdd.jl.git",
     versions = nothing,
     push_preview = true
 )
